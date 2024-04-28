fe838b473e642bab917d7029b5a4b4c8ce20189d
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2024-04-28 16:33:42 +0700

    Add Factory Pattern Simple

--- creation-group/factory-simple/Program.cs
+++ creation-group/factory-simple/Program.cs
@@ -4,6 +4,42 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World 1");
+        var productFactory = new ProductFactory();
+        var product1 = productFactory.createProduct(0);
+        var product2 = productFactory.createProduct(99.000m);
+        Console.WriteLine(product1.ToString());
+        Console.WriteLine(product2.ToString());
+    }
+}
+
+
+public class Product(string name, decimal price)
+{
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+
+    public override string ToString()
+    {
+        return String.Format($"Name: {Name} \nPrice: {Price}");
+    }
+}
+
+// Hide Business Logic To Create Product 
+public class ProductFactory
+{
+    public Product product;
+    public Product createProduct(decimal price)
+    {
+
+        switch (price)
+        {
+            case 0:
+                product = new Product("Free Product", 0);
+                break;
+            default:
+                product = new Product("Fixed Product", price);
+                break;
+        }
+        return product;
     }
 }
